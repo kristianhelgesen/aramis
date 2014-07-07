8f78085cf0217d0ac570d4f663e97621e34e42ea
Kristian Helgesen <kristian@tripletex.no>
2014-07-07 09:53:59 +0200

    testing out jexl expression parser

---
+++ src/test/java/com/github/aramis/ExpressionParserTest.java
@@ -0,0 +1,49 @@
+package com.github.aramis;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.jexl2.Expression;
+import org.apache.commons.jexl2.JexlContext;
+import org.apache.commons.jexl2.JexlEngine;
+import org.apache.commons.jexl2.ObjectContext;
+import org.junit.Test;
+
+public class ExpressionParserTest {
+
+	@Test
+	public void testPlainObjectExpression() {
+        JexlEngine jexl = new JexlEngine();
+        Expression e = jexl.createExpression( "b.c" );
+
+        JexlContext jc = new ObjectContext<A>(jexl, new A());
+        Object o = e.evaluate(jc);
+        assertEquals(o,"ccc");
+	}
+	
+	
+	public static class A{
+		public B b = new B();
+		
+		public B getB() {
+			return b;
+		}
+
+		public void setB(B b) {
+			this.b = b;
+		}
+
+		public static class B{
+			public String c = "ccc";
+
+			public String getC() {
+				return c;
+			}
+
+			public void setC(String c) {
+				this.c = c;
+			}
+			
+		}
+	}
+	
+}
